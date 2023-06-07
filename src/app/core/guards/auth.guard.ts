2c7bdb760225df6cf6449226f1179becfbb54a22
viacheslav.chopyk <viacheslav,chopyk@vakoms.com>
2023-06-07 12:50:08 +0300

    add some logic to routs

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (!localStorage.getItem('token')) {
+    if (!localStorage.getItem('tokenSniApiToken')) {
       this.router.navigateByUrl('login');
       return false;
     }
