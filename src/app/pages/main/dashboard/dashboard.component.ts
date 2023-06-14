c03998f690211358c0e6de16f1e7961740238f38
viacheslav.chopyk <viacheslav,chopyk@vakoms.com>
2023-06-14 19:15:58 +0300

    change logic

--- src/app/pages/main/dashboard/dashboard.component.ts
+++ src/app/pages/main/dashboard/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DashboardService } from './dashboard.service';
 import { IDataValue, ISocialNetwork } from './types/dashboard.types';
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -56,7 +56,6 @@ export class DashboardComponent implements OnInit {
       this.myForm[el] = this.fb.group({});
       this.dataValue[el].forEach((item: any) => {
         const validators = item.required ? [Validators.required] : [];
-
         this.myForm[el].addControl(
           item.name,
           new FormControl(item.value, validators)
@@ -65,65 +64,25 @@ export class DashboardComponent implements OnInit {
     });
   }
 
-  isInputRequired(item: any): boolean {
-    return item.required === true;
+  isInputRequired(input: any): boolean {
+    return input.required === true;
   }
 
   submitForm() {
     const tableData: Record<string, any> = {
-      data: {
-        TABLEPOSTS: {
-          
-        }
-        // {
-        //   TABLEPOSTS: {
-        //     ...this.myForm.TABLEPOSTS.value,
-        //     network: this.selectedNetwork,
-        //     //const data sent with every table
-        //     StatusData: 21,
-        //   },
-        //   TABLECOMMENT: {
-        //     ...this.myForm.TABLECOMMENT.value,
-        //     network: this.selectedNetwork,
-        //     //const data sent with every table
-        //     StatusData: 21,
-        //   },
-        //   TABLEGROUPS: {
-        //     ...this.myForm.TABLEGROUPS.value,
-        //     network: this.selectedNetwork,
-        //     //const data sent with every table
-        //     StatusData: 21,
-        //   },
-        //   TABLEUSER: {
-        //     ...this.myForm.TABLEUSER.value,
-        //     network: this.selectedNetwork,
-        //     //const data sent with every table
-        //     StatusData: 21,
-        //   },
-        // },
-      },
-  
+      data: [
+        ],
     };
     Object.keys(this.myForm).forEach(el=>{
       console.log(el);
       
-      this.tableData.data[el] = {
-        ...this.myForm[el].value,
+      tableData['data'].push( {[el]:{
+        ...this.transformData(this.myForm[el].value, el),
         network: this.selectedNetwork,
         //const data sent with every table
         StatusData: 21,
-      }
+      }})
     })
-    console.log(tableData);
-
-    // this.dataValue.forEach((el:any) => {
-    //   console.log(el);
-    //   if (el.type === 'int' || el.type === 'bigint') {
-    //     tableData[this.selectedValue][el.name] = Number(
-    //       tableData[this.selectedValue][el.name]
-    //     );
-    //   }
-    // });
 
     this.dashboardService.sendTableData(tableData).subscribe((resp) => {
       if (resp) {
@@ -133,13 +92,28 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  transformData(data: any, nameForm: any){
+    this.dataValue[nameForm].forEach((el:any)=>{
+      if(el.type === 'int' || el.type === 'bigint'){
+        data[el.name] = Number(data[el.name])
+      }
+    })
+    return data;
+  }
+
+  formValidation(){
+    let valid = false;
+    Object.keys(this.myForm).forEach(el=>{
+      if(!this.myForm[el].valid){
+        valid = true;
+      }
+    })
+    return valid;
+  }
+
   downloadFile(fileUrl: string) {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    this.http
-      .get(fileUrl, {
-        responseType: 'blob',
-        headers: headers,
-      })
+    this.http.get(fileUrl, {responseType: 'blob',headers: headers,})
       .subscribe((response) => {
         const blob = new Blob([response], { type: 'application/octet-stream' });
         const url = window.URL.createObjectURL(blob);
